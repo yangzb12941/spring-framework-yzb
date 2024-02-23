001132548644b12c6c4756f35bea388c05d429a6
yangzb <1294043509@qq.com>
2024-02-23 17:15:51 +0800

    2024-02-23： 1、ProxyConfig 设置 exposeProxy 属性为true

--- spring-aop/src/main/java/org/springframework/aop/framework/ProxyConfig.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/ProxyConfig.java
@@ -33,15 +33,23 @@ public class ProxyConfig implements Serializable {
 	/** use serialVersionUID from Spring 1.2 for interoperability. */
 	private static final long serialVersionUID = -8409359707199703185L;
 
-
+	//标记是否直接对目标类进行代理，而不是通过接口产生代理
+	//或者说，标记是否使用CGLIB动态代理，true，表示使用CGLIB的方式产生代理对象，false，表示使用JDK动态代理
 	private boolean proxyTargetClass = false;
 
+	// 标记是否进行优化。启动优化通常意味着在代理对象被创建后，增强的修改将不会生效，因此默认值为false。
+	// 如果exposeProxy设置为true，即使optimize为true也会被忽略。
 	private boolean optimize = false;
 
+	// 标记是否需要阻止通过该配置创建的代理对象转换为Advised类型，默认值为false，表示代理对象可以被转换为Advised类型
 	boolean opaque = false;
 
+	// 标记代理对象是否应该被aop框架通过AopContext以ThreadLocal的形式暴露出去。
+	// 当一个代理对象需要调用它自己的另外一个代理方法时，这个属性将非常有用。默认是是false，以避免不必要的拦截。
 	boolean exposeProxy = false;
 
+	// 标记该配置是否需要被冻结，如果被冻结，将不可以修改增强的配置。
+	// 当我们不希望调用方修改转换成Advised对象之后的代理对象时，这个配置将非常有用。
 	private boolean frozen = false;
 
 
--- spring-test/src/test/java/org/springframework/test/yzbtest/proxy/AspectJAopTest.java
+++ spring-test/src/test/java/org/springframework/test/yzbtest/proxy/AspectJAopTest.java
@@ -1,11 +1,12 @@
 package org.springframework.test.yzbtest.proxy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.AopContext;
+import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.yzb.InitializingDisposableBean.AInitializingBean;
 import org.springframework.test.yzb.DynamicProxy.SpringJDKProxy.AspectJAop.AspectJAopTarget;
 import org.springframework.test.yzb.DynamicProxy.SpringJDKProxy.utils.TestResourceUtils;
 
@@ -15,7 +16,7 @@ public class AspectJAopTest {
     //没有切面效果
 	//
     @Test
-    public void aspectJAopTest_1() throws IOException {
+    public void aspectJAopTest_1(){
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         new XmlBeanDefinitionReader(beanFactory).loadBeanDefinitions(
                 TestResourceUtils.qualifiedResource(AspectJAopTest.class, "1.xml"));
@@ -30,17 +31,15 @@ public class AspectJAopTest {
     //有切面效果
     @Test
     public void aspectJAopTest_2() throws IOException {
-        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:org/springframework/test/proxy/AspectJAopTest-1.xml");
-        AspectJAopTarget aspectJAopTarget = ac.getBean("aspectJAopTarget", AspectJAopTarget.class);
-        aspectJAopTarget.aopMethodBefore();
-        aspectJAopTarget.aopMethodAfter();
-    }
+        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:org/springframework/test/yzbtest/proxy/AspectJAopTest-1.xml");
+        AspectJAopTarget aspectJAopTargetA = ac.getBean("aspectJAopTarget", AspectJAopTarget.class);
 
-	//InitializingBean 和 DisposableBean 接口调用时机验证
-	@Test
-	public void aInitializingBeanTest() throws IOException {
-		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("classpath:org/springframework/test/proxy/AInitializingBeanTest.xml");
-		AInitializingBean aInitializingBean = ac.getBean("aInitializingBean", AInitializingBean.class);
-		ac.close();
-	}
+		aspectJAopTargetA.aopMethodBefore();
+        aspectJAopTargetA.aopMethodAfter();
+
+		ProxyFactory proxyFactory = ac.getBean("proxyFactory", ProxyFactory.class);
+		proxyFactory.setExposeProxy(Boolean.TRUE);
+		AspectJAopTarget aspectJAopTargetB = (AspectJAopTarget)AopContext.currentProxy();
+		aspectJAopTargetB.aopMethodBefore();
+    }
 }
